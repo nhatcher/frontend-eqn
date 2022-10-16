0334d284c254ef29b56e1cdd391f32b78d812fc2
Nicolás Hatcher <nicolas@theuniverse.today>
2022-10-16 13:18:13 +0200

    FIX: more typos

--- html/main.js
+++ html/main.js
@@ -2,9 +2,9 @@ import calculate from './calculator.js'
 
 function setUpWindow() {
     const caculator = document.getElementById('calculator');
-    const display = document.getElementById('calc-display');
+    const display = document.getElementById('eqn-display');
     caculator.addEventListener('click', (event) => {
-        if (event.target.classList.contains('calc-button')) {
+        if (event.target.classList.contains('eqn-button')) {
             const wholeText = display.innerText;
             const text = calculate(event.target.innerText, wholeText);
             display.innerText = text;
